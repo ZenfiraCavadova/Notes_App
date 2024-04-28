e381b59b81c30caaef30f13eabed9bacecc992eb
natighajiyev <natig.hajiyev1@gmail.com>
2024-04-28 23:06:29 +0400

    Bugfix

--- domain/src/main/java/com/example/domain/entities/response_models/GetAllNotesResponseModel.kt
+++ domain/src/main/java/com/example/domain/entities/response_models/GetAllNotesResponseModel.kt
@@ -2,10 +2,10 @@ package com.example.domain.entities.response_models
 
 import com.google.gson.annotations.SerializedName
 
-data class GetAllNotesResponseModel(
-    @SerializedName("notes")
-    val notes:List<NoteResponseModel>
-)
+//data class GetAllNotesResponseModel(
+//    @SerializedName("notes")
+//    val notes:List<NoteResponseModel>
+//)
 data class NoteResponseModel(
     @SerializedName("title")
     val title: String,
--- domain/src/main/java/com/example/domain/repositories/NoteRepository.kt
+++ domain/src/main/java/com/example/domain/repositories/NoteRepository.kt
@@ -3,12 +3,13 @@ package com.example.domain.repositories
 import androidx.lifecycle.LiveData
 import com.example.domain.entities.Note
 import com.example.domain.entities.request_models.NoteRequestModel
-import com.example.domain.entities.response_models.GetAllNotesResponseModel
+//import com.example.domain.entities.response_models.GetAllNotesResponseModel
+import com.example.domain.entities.response_models.NoteResponseModel
 import kotlinx.coroutines.flow.Flow
 
 interface NoteRepository {
 
-    suspend fun loadNotes():GetAllNotesResponseModel
+    suspend fun loadNotes(): List<NoteResponseModel>
 
     suspend fun addNoteRemote(noteRequestModel: NoteRequestModel)
     fun saveNote(note: Note)
